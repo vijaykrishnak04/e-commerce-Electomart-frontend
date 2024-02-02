e302036738c405d669d3d62608c32622a2f30679
Muhmmed Shifin P <muhammedshifin424@gmail.com>
2024-02-03 00:49:25 +0530

    fetched the user data and listed the data.

--- src/components/admin/AdminUserManagement.jsx
+++ src/components/admin/AdminUserManagement.jsx
@@ -24,34 +24,32 @@ const AdminUserManagement = () => {
             Cell: ({ row }) => row.index + 1,
         },
         { Header: "Full Name", accessor: "fullName" },
-        { Header: "Email", accessor: "email" }, 
+        { Header: "Email", accessor: "email" },
         { Header: "Phone", accessor: "mobile" },
         {
             Header: "Actions",
             accessor: "Actions",
             Cell: ({ row }) => (
                 <div className="flex justify-center">
-                    <BiSolidEdit
-                        className="text-blue-500 w-10 h-7 cursor-pointer ml-2"
-                        onClick={() => handleEdit(row?.original?.id)}
-                    />
+                    <Button className="px-5 py-1 font-poppins font-semibold text-opacity-10 rounded border border-black
+                     shadow-md bg-green-50 hover:bg-green-40" text="Active" />
                 </div>
             ),
         },
     ];
-    
 
-return (
-    <>
-        <div className="flex justify-between item-center p-4">
-            <h1 className="text-black text-2xl font-poppins font-Bold">Users</h1>
-        </div>
-        <div className="mt-5 border  border-black p-5 rounded text-center mx-auto bg-white">
-            <DataTable columns={columns} data={Array.isArray(userData) ? userData : [userData]} />
-        </div>
 
-    </>
-)
+    return (
+        <>
+            <div className="flex justify-between item-center p-4">
+                <h1 className="text-black text-2xl font-poppins font-Bold">Users</h1>
+            </div>
+            <div className="mt-5 border  border-black p-5 rounded text-center mx-auto bg-white">
+                <DataTable columns={columns} data={Array.isArray(userData) ? userData : [userData]} />
+            </div>
+
+        </>
+    )
 }
 
 export default AdminUserManagement;
---
+++ src/pages/admin/AdminOrderManagementPage.jsx
@@ -0,0 +1,11 @@
+import AdminOrderManagement from "../../components/admin/AdminOrderManagement"
+
+const AdminOrderManagementPage = () => {
+    return (
+        <div>
+            <AdminOrderManagement />
+        </div>
+    )
+}
+
+export default AdminOrderManagementPage;
