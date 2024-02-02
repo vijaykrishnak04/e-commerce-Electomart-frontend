c1c8f5e7880d346f2905c787edb03b9743a4d0cc
Muhmmed Shifin P <muhammedshifin424@gmail.com>
2024-02-02 19:59:15 +0530

    updated the store

--- src/app/store/store.jsx
+++ src/app/store/store.jsx
@@ -1,15 +1,16 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { bannerSlice } from '../slices/admin/adminBannerSlice';
 import { CategorySlice } from '../slices/admin/adminCategorySlice';
+import { subcategorySlice} from '../slices/admin/adminSubcategorySlice';
 
 
 const rootReducer = combineReducers({
-    Banner: bannerSlice.reducer,
-    Category: CategorySlice.reducer 
-   
-  });
+  Banner: bannerSlice.reducer,
+  Category: CategorySlice.reducer,
+  Subcategory: subcategorySlice.reducer
+});
 
 
 export const store = configureStore({
-    reducer: rootReducer
+  reducer: rootReducer
 })
